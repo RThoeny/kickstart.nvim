08bf7b3586697f67362349600fe5371c199f9e39
Reid Thoeny <reidthoeny@gmail.com>
2025-01-18 09:25:18 -0600

    Add formatting rules to init.lua

--- init.lua
+++ init.lua
@@ -970,5 +970,11 @@ require('lazy').setup({
   },
 })
 
+vim.o.tabstop = 3
+vim.o.softtabstop = 3
+vim.o.shiftwidth = 3
+vim.o.expandtab = true
+vim.o.smartindent = true
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
